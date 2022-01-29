13888b8b024a7b8741d252b2c95daebc85b8606b
rubenavaldez <rubenavaldez@yahoo.com>
2022-01-28 22:02:32 -0700

    added read line

--- Number_Guesser/Program.cs
+++ Number_Guesser/Program.cs
@@ -19,7 +19,42 @@ namespace Number_Guesser
             string appName = "Number Guesser";
             string appVersion = "1.0.0";
             string appAuthor = "Ruben Valdez";
+
+            //Change text color 
+            Console.ForegroundColor = ConsoleColor.Green;
+
+            //Write out app info
             Console.WriteLine("{0} version {1} by {2}", appName, appVersion, appAuthor);
+
+            //Reset text color
+            Console.ResetColor();
+
+            // Ask user's name 
+            Console.WriteLine("What is your name");
+
+            //Get user input
+            string input = Console.ReadLine();
+
+            Console.WriteLine("Hello {0}, I'd like to play a game", input);
+
+            //Set correct number
+            int correctNumber = 7;
+
+            //init guess var
+            int guess = 0;
+
+            //ask user for number
+            Console.WriteLine("Guess a number between 1 and 10");
+
+            //while guess is not correct
+            while (guess != correctNumber) { 
+            
+                guess = Console.ReadLine();
+            }
+    
+
+
+
         }
     }
 }
