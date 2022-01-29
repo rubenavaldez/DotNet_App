beeaf5b7598cdc7028c3b1b1e462cff22f15fc84
rubenavaldez <rubenavaldez@yahoo.com>
2022-01-28 21:39:28 -0700

    set up environment

---
+++ Number_Guesser/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+// Namespace
+namespace Number_Guesser
+{
+    //Main Class
+    internal class Program
+    {
+        // Entry Point Method  // Void is the return type //Static means refers to the function instead of an instance of an object
+        static void Main(string[] args)
+        {
+            //  string name = "Good Morning";
+            //int age = 38;
+
+            //Ctrl f5 to run
+            //Console.WriteLine("hello and " + name + " you are " +age+ " years old");
+            //Console.WriteLine("{0} you are {1}", name, age);
+
+            string appName = "Number Guesser";
+            string appVersion = "1.0.0";
+            string appAuthor = "Ruben Valdez";
+            Console.WriteLine("{0} version {1} by {2}", appName, appVersion, appAuthor);
+        }
+    }
+}
---
+++ Number_Guesser/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Number_Guesser")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Number_Guesser")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("448f1d80-53a0-493d-b9ff-00fa9a4ef29a")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
